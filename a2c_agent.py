226930e301200f9596e8e9afd008b46667fcc7d2
jinkim31 <jinkim5771@gmail.com>
2022-04-28 16:46:15 +0900

    core comprehensive batch. added comments

--- a2c_agent.py
+++ a2c_agent.py
@@ -28,16 +28,13 @@ class Actor(Model):
         std = self.std(x)
 
         # Scale output to [-action_bound, action_bound]
-        mu = Lambda(lambda x: x*self.action_bound)(mu)
+        mu = Lambda(lambda x: x * self.action_bound)(mu)
 
         return [mu, std]
 
 
 # critic network
 class Critic(Model):
-    """
-        Critic Network for A2C: V function approximator
-    """
     def __init__(self):
         super(Critic, self).__init__()
 
@@ -54,7 +51,6 @@ class Critic(Model):
         return v
 
 
-# a2c agent for training
 class A2Cagent(object):
 
     def __init__(self, env):
@@ -65,49 +61,43 @@ class A2Cagent(object):
         self.ACTOR_LEARNING_RATE = 0.0001
         self.CRITIC_LEARNING_RATE = 0.001
 
+        # environment
         self.env = env
-        # get state dimension
         self.state_dim = env.observation_space.shape[0]
-        # get action dimension
         self.action_dim = env.action_space.shape[0]
-        # get action bound
         self.action_bound = env.action_space.high[0]
-        self.std_bound = [1e-2, 1.0]  # std bound
+        self.std_bound = [1e-2, 1.0]
 
-        # create actor and critic networks
+        # networks
         self.actor = Actor(self.action_dim, self.action_bound)
         self.critic = Critic()
         self.actor.build(input_shape=(None, self.state_dim))
         self.critic.build(input_shape=(None, self.state_dim))
-
         self.actor.summary()
         self.critic.summary()
 
-        # optimizer
+        # optimizers
         self.actor_opt = adam_v2.Adam(self.ACTOR_LEARNING_RATE)
         self.critic_opt = adam_v2.Adam(self.CRITIC_LEARNING_RATE)
 
         # save the results
         self.save_epi_reward = []
 
-
-    ## log policy pdf
+    # log of gaussian probability density function
     def log_pdf(self, mu, std, action):
         std = tf.clip_by_value(std, self.std_bound[0], self.std_bound[1])
         var = std ** 2
         log_policy_pdf = -0.5 * (action - mu) ** 2 / var - 0.5 * tf.math.log(var * 2 * np.pi)
         return tf.reduce_sum(log_policy_pdf, 1, keepdims=True)
 
-
-    ## actor policy
     def get_action(self, state):
         mu_a, std_a = self.actor(state)
-        mu_a = mu_a.numpy()[0]
+        mu_a = mu_a.numpy()[0]  # unpack outer []
         std_a = std_a.numpy()[0]
         std_a = np.clip(std_a, self.std_bound[0], self.std_bound[1])
+        # sample action
         action = np.random.normal(mu_a, std_a, size=self.action_dim)
         return action
-
 
     ## train the actor network
     def actor_learn(self, states, actions, advantages):
@@ -124,16 +114,14 @@ class A2Cagent(object):
         grads = tape.gradient(loss, self.actor.trainable_variables)
         self.actor_opt.apply_gradients(zip(grads, self.actor.trainable_variables))
 
-
     ## single gradient update on a single batch data
     def critic_learn(self, states, td_targets):
         with tf.GradientTape() as tape:
             td_hat = self.critic(states, training=True)
-            loss = tf.reduce_mean(tf.square(td_targets-td_hat))
+            loss = tf.reduce_mean(tf.square(td_targets - td_hat))
 
         grads = tape.gradient(loss, self.critic.trainable_variables)
         self.critic_opt.apply_gradients(zip(grads, self.critic.trainable_variables))
-
 
     ## computing targets: y_k = r_k + gamma*V(x_k+1)
     def td_target(self, rewards, next_v_values, dones):
@@ -145,80 +133,76 @@ class A2Cagent(object):
                 y_i[i] = rewards[i] + self.GAMMA * next_v_values[i]
         return y_i
 
-
     ## load actor wieghts
     def load_weights(self, path):
         self.actor.load_weights(path + 'pendulum_actor.h5')
         self.critic.load_weights(path + 'pendulum_critic.h5')
 
-
     ## convert (list of np.array) to np.array
     def unpack_batch(self, batch):
         unpack = batch[0]
-        for idx in range(len(batch)-1):
-            unpack = np.append(unpack, batch[idx+1], axis=0)
-
+        for idx in range(len(batch) - 1):
+            unpack = np.append(unpack, batch[idx + 1], axis=0)
         return unpack
 
-
-    ## train the agent
     def train(self, max_episode_num):
 
         for ep in range(int(max_episode_num)):
 
-            # initialize batch
+            # batches. use mutable array for efficiency. use unpack() to convert back to immutable tf tensor.
             batch_state, batch_action, batch_reward, batch_next_state, batch_done = [], [], [], [], []
-            # reset episode
+
             time, episode_reward, done = 0, 0, False
-            # reset the environment and observe the first state
-            state = self.env.reset() # shape of state from gym (3,)
+            state = self.env.reset()
 
             while not done:
+                # self.env.render()
 
-                # visualize the environment
-                #self.env.render()
-
-                # reshape of state (state_dim,) -> (1,state_dim)
+                # first layer of actor(dense) requires 2 dim tensor at least. add a rank on state with []
                 action = self.get_action(tf.convert_to_tensor([state], dtype=tf.float32))
-                # clip continuous action to be within action_bound
+
+                # action is sampled value from gaussian(mu, std)
                 action = np.clip(action, -self.action_bound, self.action_bound)
-                # observe reward, new_state, shape of output of gym (state_dim,)
+
+                # observe
                 next_state, reward, done, _ = self.env.step(action)
 
-                # change shape (state_dim,) -> (1, state_dim), same to action, next_state
-                state = np.reshape(state, [1, self.state_dim])
-                action = np.reshape(action, [1, self.action_dim])
-                reward = np.reshape(reward, [1, 1])
-                next_state = np.reshape(next_state, [1, self.state_dim])
-                done = np.reshape(done, [1, 1])
+                # reshape 1d array to 2d row column vector.
+                state = np.array(state)
+                action = np.array(action)
+                reward = np.array(reward)
+                next_state = np.array(next_state)
+                done = np.array(done)
 
                 # compute advantage and TD target
                 train_reward = (reward + 8) / 8  # <-- normalization
 
-                # append to the batch
-                batch_state.append(state)
-                batch_action.append(action)
-                batch_reward.append(train_reward)
-                batch_next_state.append(next_state)
-                batch_done.append(done)
+                # append to the batches(mutable arrays for efficiency)
+                # batches can be converted back to immutable tf tensors using unpack()
+                # [1 2 3].append([4 5 6]) returns [1 2 3 4 5 6] which eliminates borders between states/actions...
+                # use [] to address the problem
+                batch_state.append([state])
+                batch_action.append([action])
+                batch_reward.append([train_reward])
+                batch_next_state.append([next_state])
+                batch_done.append([done])
 
-                # continue until batch becomes full
+                # check if batches are full
                 if len(batch_state) < self.BATCH_SIZE:
-                    # update current state
-                    state = next_state[0]
-                    episode_reward += reward[0]
+                    # batch not full yet
+                    state = next_state
+                    episode_reward += reward
                     time += 1
                     continue
 
-                # if batch is full, start to train networks on batch
-                # extract batched states, actions, ...
+                # batches full. not continued
                 states = self.unpack_batch(batch_state)
                 actions = self.unpack_batch(batch_action)
                 train_rewards = self.unpack_batch(batch_reward)
                 next_states = self.unpack_batch(batch_next_state)
                 dones = self.unpack_batch(batch_done)
 
-                # clear the batch
+                # clear batches
                 batch_state, batch_action, batch_reward, batch_next_state, batch_done = [], [], [], [], []
 
                 # compute next v_value with previous V estimate
@@ -240,26 +224,22 @@ class A2Cagent(object):
                                  tf.convert_to_tensor(advantages, dtype=tf.float32))
 
                 # update current state
-                state = next_state[0]
-                episode_reward += reward[0]
+                state = next_state
+                episode_reward += reward
                 time += 1
 
-
             ## display rewards every episode
-            print('Episode: ', ep+1, 'Time: ', time, 'Reward: ', episode_reward)
+            print('Episode: ', ep + 1, 'Time: ', time, 'Reward: ', episode_reward)
 
             self.save_epi_reward.append(episode_reward)
-
 
             ## save weights every episode
             if ep % 10 == 0:
                 self.actor.save_weights("./save_weights/pendulum_actor.h5")
                 self.critic.save_weights("./save_weights/pendulum_critic.h5")
 
-
         np.savetxt('./save_weights/pendulum_epi_reward.txt', self.save_epi_reward)
         print(self.save_epi_reward)
-
 
     ## save them to file if done
     def plot_result(self):
